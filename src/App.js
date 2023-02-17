e554d85d509f00d21dfae42f667902ceb1be0de6
pranav <pranavbavaskar1@gmail.com>
2023-02-17 18:59:58 +0530

    react handson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Classcomponent from "./classbasedcomponents";
+import Functioncomponent from "./functionbasedcomponents"
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [funcClick, setfuncClick]=useState(false);
+
+
+const handleFuncClick=()=>{
+  setfuncClick(!funcClick)
+}
+  
+const [compoClick,setcompoClick]=useState(false);
+
+const handlecompoClick=()=>{
+  setcompoClick(!compoClick)
+}
+
+
+  return(
+    <>
+    <div className="body">
+    <h1>{funcClick}</h1>
+  <h2 className="mainheading">Styling using Functional and Class Component</h2>
+  <button onClick={handleFuncClick} className='btn1'>To see styling in functional component</button>
+  <button onClick={handlecompoClick} className='btn2'> To see styling in class component</button>
+  {funcClick &&<Functioncomponent/> }
+  {compoClick &&<Classcomponent/> }
+  
+
+  </div>
+    </>
+  )
+  
 }
 
 export default App;
